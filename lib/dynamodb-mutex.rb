13f7e921074cde0f27bda02a88ddfe5cf51c66b8
Casper Thomsen <ct@clearhaus.com>
2014-12-10 09:57:08 +0100

    White-space clean-up.

--- lib/dynamodb-mutex.rb
+++ lib/dynamodb-mutex.rb
@@ -1,2 +1,1 @@
-
 require_relative 'dynamodb_mutex'
--- lib/dynamodb_mutex.rb
+++ lib/dynamodb_mutex.rb
@@ -1,8 +1,7 @@
-
 require 'dynamodb_mutex/lock'
 
 module DynamoDBMutex
-  
+
   def self.with_lock *args, &block
     Lock.with_lock(*args, &block)
   end
