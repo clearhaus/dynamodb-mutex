de6e3e2109488c3550d6d7327b1b1406e04b37a7
Kasper Sacharias Eenberg <kse@clearhaus.com>
2018-05-07 11:29:34 +0200

    Fix bug in wait-loop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,20 +5,20 @@ require 'rspec'
 require 'English'
 
 require 'aws-sdk-dynamodb'
+require 'dynamodb-mutex'
 
-Aws.config.update({
+Aws.config.update(
   credentials: Aws::Credentials.new(
     'your_access_key_id',
     'your_secret_access_key'
   ),
   endpoint: 'http://localhost:4567'
-})
+)
 
 AMAZON_LOCAL_DYNAMODB_URL =
   'https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt/dynamodb_local_latest.tar.gz'.freeze
 DYNAMODB_JAR_DIR = 'resources'.freeze
 DYNAMODB_TMP_PATH = '/tmp/dynamodb_local_latest.tar.gz'.freeze
-require 'dynamodb-mutex'
 
 RSpec.configure do |config|
   log_stream = ENV['DEBUG'] =~ (/^(true|t|yes|y|1)$/i) ? STDERR : StringIO.new
@@ -27,7 +27,7 @@ RSpec.configure do |config|
 
   dynamo_pid = nil
 
-  config.before(:all) do
+  config.before(:suite) do
     # Download Amazons local DynamoDB and use that as an endpoint for specs.
 
     Dir.mkdir(DYNAMODB_JAR_DIR) unless Dir.exist?(DYNAMODB_JAR_DIR)
@@ -54,6 +54,7 @@ RSpec.configure do |config|
     sleep_interval = 0.1
 
     while loop_count < loop_max_wait / sleep_interval
+      loop_count += 1
       begin
         client = Aws::DynamoDB::Client.new
         client.list_tables
@@ -67,7 +68,7 @@ RSpec.configure do |config|
     raise 'DynamoDB did not start in time' if loop_count >= loop_max_wait / sleep_interval
   end
 
-  config.after(:all) do
+  config.after(:suite) do
     Process.kill('INT', dynamo_pid) if dynamo_pid
     Process.waitpid(dynamo_pid)
   end
